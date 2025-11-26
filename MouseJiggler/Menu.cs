f6cca6502a115143b5cedbfb7170b6dc0de35df0
Chester J. Ayala <chesterayala.ca@gmail.com>
2025-11-26 15:01:09 +0800

    Initialize project

---
+++ MouseJiggler/Menu.cs
@@ -0,0 +1,15 @@
+namespace MouseJiggler
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
